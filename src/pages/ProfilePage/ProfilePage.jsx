d081de5ce1f68a8a292d7680914854c19ef2b821
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-31 16:30:05 +0200

    profile on it

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,43 @@
+import { useContext, useEffect, useState } from "react";
 import "./ProfilePage.css";
+import { AuthContext } from "../../context/auth.context";
+
 
 function ProfilePage() {
+
+  const {user} = useContext(AuthContext)
+  const [items, setItems] = useState([])
+
+  useEffect(()=>{
+
+    const itemUrl = process.env.REACT_APP_SERVER_URL + "/db/items/owner/" + user._id
+    
+    fetch(itemUrl)
+    .then((response)=>{
+      return response.json()
+    })
+    .then((data)=>{
+      console.log(data);
+      setItems(data)
+    })
+    .catch(err => console.log(err))
+
+},[])
+  
+
   return (
-    <div>
+    <div className="profile-page">
       <h1>Profile page</h1>
+
+      <div className="profile-details">
+         <h3>{user.username}</h3>
+         <p><strong>Email:</strong> {user.email}</p>
+      </div>
+
+      <div className="user-products">
+        <h2>{user.username}'s Listing</h2>
+          
+      </div>
     </div>
   );
 }
