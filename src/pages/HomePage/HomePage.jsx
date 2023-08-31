8cc5e424a37ac33ee988dac21201db27b38c766e
Oscar Mesejo <oscar.mesejo@gmail.com>
2023-08-31 16:18:04 +0200

    news.map done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,29 +1,12 @@
 import "./HomePage.css";
-import { useState, useEffect } from "react";
-
 
 function HomePage() {
-
-  const [news, setNews] = useState([]);
-
-  useEffect(()=>{
-    const newsUrl = process.env.REACT_APP_SERVER_URL + "/app/news/"
-
-    fetch(newsUrl)
-    .then((response)=>{
-      return response.json()
-    })
-    .then((data)=>{
-      console.log(data);
-    })
-    .catch(err => console.log(err));
-  }, [])
-
+  
   return (
     <div>
       <h1>Home page</h1>
-     
-     <NewsList/>
+
+      <NewsList />
 
     </div>
   );
