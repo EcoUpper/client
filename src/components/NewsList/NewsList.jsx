de8edb52ffd62ec255b5e7ee3161bf5fbf4b67db
ArvidErik <erik.arvid.hosszu@gmail.com>
2023-08-31 17:42:45 +0200

    footer fixed

--- src/components/NewsList/NewsList.jsx
+++ src/components/NewsList/NewsList.jsx
@@ -1,36 +1,30 @@
 import { useState, useEffect } from "react";
 
 function NewsList() {
-    const [news, setNews] = useState([]);
+  const [news, setNews] = useState([]);
 
-    useEffect(() => {
-        const newsUrl = process.env.REACT_APP_SERVER_URL + "/api/news"
+  useEffect(() => {
+    const newsUrl = process.env.REACT_APP_SERVER_URL + "/api/news";
 
-        fetch(newsUrl)
-            .then((response) => {
-                return response.json()
-            })
-            .then((data) => {
-                console.log(data);
-                setNews(data);
-            })
-            .catch(err => console.log(err));
-    }, [])
+    fetch(newsUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setNews(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    return (
-        <>
-
-        <h3>NEWS</h3>
-        {
-            news.map((news)=>{
-                return(
-                    <div className="news"></div>
-                )
-            })
-        }
-
-        </>
-    )
+  return (
+    <>
+      <h3>NEWS</h3>
+      {news.map((news) => {
+        return <div className="news"></div>;
+      })}
+    </>
+  );
 }
 
 export default NewsList;
