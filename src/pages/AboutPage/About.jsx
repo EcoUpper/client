7bf6932ba1276086462b23fb7cf0cda48919848d
Juan David Valencia <juandavalg@gmail.com>
2023-09-01 10:51:38 +0200

    About Page

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,9 +1,43 @@
 function About (){
     return (
         <>
+        <h1>About Page</h1>
+        <p>This is the About Page</p>
+        <section>
+    <h2>Our Story</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in mauris eu nulla convallis tristique. Sed semper massa at dui volutpat, vitae finibus lorem aliquam. Curabitur blandit, ex eu tristique feugiat, purus arcu aliquet ante, nec feugiat nisi ante eu ipsum.</p>
+  </section>
+  
+  <section>
+    <h2>Our Team</h2>
+    <div class="team-member">
+      <img src="" alt=""/>
+      <h3>Edna Vidal</h3>
+      <p>Position: CEO</p>
+    </div>
+    <div class="team-member">
+      <img src="" alt=""/>
+      <h3>Erik Arvid</h3>
+      <p>Position: CTO</p>
+    </div>
+
+    <div class="team-member">
+      <img src="" alt=""/>
+      <h3>Óscar mesejo</h3>
+      <p>Position: Crack</p>
+    </div>
+
+    <div class="team-member">
+      <img src="" alt=""/>
+      <h3>JuanDa</h3>
+      <p>Position: Doorman</p>
+    </div>
+  </section>
+
+
         </>
     )
 
-}
+};
 
 export default About;
