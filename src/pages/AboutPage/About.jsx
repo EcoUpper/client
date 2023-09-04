f2044bac3cfac74eca2d1998a444b6c268d99944
Juan David Valencia <juandavalg@gmail.com>
2023-09-04 10:01:38 +0200

    Adding sources

--- src/pages/AboutPage/About.jsx
+++ src/pages/AboutPage/About.jsx
@@ -1,3 +1,5 @@
+import "./AboutPage.css"
+
 function About (){
     return (
         <>
@@ -11,24 +13,24 @@ function About (){
   <section>
     <h2>Our Team</h2>
     <div class="team-member">
-      <img src="" alt=""/>
+      <img src="Edna.JPG" alt=""/>
       <h3>Edna Vidal</h3>
       <p>Position: CEO</p>
     </div>
     <div class="team-member">
-      <img src="" alt=""/>
+      <img src="Erik.JPG" alt=""/>
       <h3>Erik Arvid</h3>
       <p>Position: CTO</p>
     </div>
 
     <div class="team-member">
-      <img src="" alt=""/>
+      <img src="Oscar.JPG" alt=""/>
       <h3>Óscar Mesejo</h3>
-      <p>Position: Crack</p>
+      <p>Position: Accountant</p>
     </div>
 
     <div class="team-member">
-      <img src="" alt="Doorman photo"/>
+      <img src="JD.JPG" alt="Doorman photo"/>
       <h3>JuanDa</h3>
       <p>Position: Doorman</p>
     </div>
