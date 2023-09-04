43d37d7baeeb27922994a308deac08bc4b1241a6
Edna Vidal <ednavidalros@gmail.com>
2023-09-04 12:51:25 +0200

    "re-factoring and adding sorted/updated various"

--- src/components/Market/ItemsList.jsx
+++ src/components/Market/ItemsList.jsx
@@ -1,62 +1,22 @@
-import { useState, useEffect } from "react"
-import { Link } from "react-router-dom"
+import ItemCard from "./ItemCard"
 
-function ItemsList() {
-    const [items, setItems] = useState([])
-    const apiUrl = process.env.REACT_APP_SERVER_URL + "/db/items"
 
-    useEffect(() => {
-        fetch(apiUrl)
-            .then((res) => {
-                return res.json()
-            })
-            .then((data) => {
-                setItems(data)
-                console.log(data)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }, [])
+function ItemsList(props) {
 
-    if (!items) {
-        return <p>Loading...</p>
-    }
+    const {items} = props
+
 
     return (
         <>
             <div>
                 {items.map((item) => {
-                     const dateAndTimePropEx = item.expiration_date
-                     const dateTimeEx = new Date(dateAndTimePropEx)
+                    const dateAndTimePropEx = item.expiration_date
+                    const dateTimeEx = new Date(dateAndTimePropEx)
 
-                     const expirationDate = dateTimeEx.toLocaleDateString()
+                    const expirationDate = dateTimeEx.toLocaleDateString()
 
                     return (
-                        <div style={{ width: "400px" }}>
-                            <Link key={item._id} to={`/items/${item._id}`}><h3>{item.name}</h3></Link>
-                            <h4><strong>{item.status}</strong></h4>
-                            <img style={{ height: "300px" }} src={item.image_url} alt="Item image" />
-                            <p>{item.description}</p>
-                            {item.type === "food" && <p>{expirationDate}</p>}
-                            <h4>Current Proposals</h4>
-                            {item.proposals?.map((proposal) => {
-                                const dateAndTimeProp = proposal.date
-                                const dateTime = new Date(dateAndTimeProp)
-
-                                const date = dateTime.toLocaleDateString()
-                                const time = dateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
-                                return (
-                                    <div key={proposal._id}>
-                                        <p>{date} at {time}</p>
-                                        <p>{proposal.status}</p>
-                                    </div>
-                                )
-                            })
-                            }
-                            {/* <p>{item.owner?.username}</p> */}
-                        </div>
-
+                        <ItemCard item={item} expirationDate={expirationDate}/>
                     )
                 })}
             </div>
--- src/pages/MarketPage/MarketPage.jsx
+++ src/pages/MarketPage/MarketPage.jsx
@@ -1,13 +1,52 @@
 import ItemsList from "./../../components/Market/ItemsList"
 import NewItem from "./../../components/Market/NewItem"
+import { useState, useEffect } from "react"
 
 
 function MarketPage() {
     
+    const [items, setItems] = useState([])
+    const apiUrl = process.env.REACT_APP_SERVER_URL + "/db/items"
+    
+    function fetchItems() {
+        fetch(apiUrl)
+            .then((res) => {
+                return res.json()
+            })
+            .then((data) => {
+                const datedItems = data.map((item) => ({
+                    ...item,
+                    createdAt: new Date(item.createdAt)
+                }));
+
+                const sortedItems = datedItems.sort((a, b) => {
+                    if (a.createdAt < b.createdAt) {
+                        return 1;
+                    } else if (a.createdAt > b.createdAt) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                });
+                setItems(sortedItems);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+    
+    useEffect(() => {
+        fetchItems()
+    }, [])
+    
+    if (!items) {
+        return <p>Loading...</p>
+    }
+
     return (
         <>
-            <NewItem/>
-            <ItemsList/>
+            <NewItem fetchItems={fetchItems}/>
+            <ItemsList items={items}/>
         </>
     )
 }
