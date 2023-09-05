e89bc2725c737c6b3bca8d7bc30d541c3d441561
Edna Vidal <ednavidalros@gmail.com>
2023-09-05 11:34:13 +0200

    "new proposal updated and visual"

--- src/components/Proposal/ProposalCard.jsx
+++ src/components/Proposal/ProposalCard.jsx
@@ -2,11 +2,7 @@ import { Link } from "react-router-dom";
 
 function ProposalCard(props) {
 
-    console.log("props are", props.data);
-
     const {time, date} = props
-
-    console.log("time", time);
 
     return (    
             <div className="proposal-card" key={props.data._id}>
