30b4f1468fcb16ec19af1418b61b4136cb27e2fc
Edna Vidal <ednavidalros@gmail.com>
2023-09-06 09:36:11 +0200

    "navbar news styles"

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,12 +2,17 @@ import "./HomePage.css"
 import "./../../components/NewsList/News.css"
 import NewsList from "./../../components/NewsList/NewsList"
 
+
 function HomePage() {
 
   return (
-    <div className="newsContainer">
-      <NewsList />
-    </div>
+
+    <>
+      {/* <div className="treesBackground"></div> */}
+      <div className="newsContainer">
+        <NewsList />
+      </div>
+    </>
   );
 }
 
