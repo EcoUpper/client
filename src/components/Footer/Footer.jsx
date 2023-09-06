17d0e04bd356fa0a89411bfe062e1a1b59df0826
Juan David Valencia <juandavalg@gmail.com>
2023-09-06 17:34:59 +0200

    footer css donde (pair programming JuanDa Oscar)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Footer.css";
+import mailImg from "./../../images/mail.svg";
+import IGImg from "./../../images/instagram.svg";
+import FBImg from "./../../images/facebook.svg";
+import tiktokImg from "./../../images/tiktok.svg";
+import linkedinImg from "./../../images/linkedin.svg";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="container">
-            <p>2023 EcoUpper. All rights reserved</p>
-            <Link to="/About">About Us</Link>
-            </div>
-        </footer>
-    )
-}
+  return (
+    <footer className="footer">
+      <div className="footerContainer">
+        <div>
+          <h3>Contact</h3>
+          <img width="30px" src={mailImg} alt="logo mail" />
+          <p>ecoupper@ecoupper.com</p>
+          <p>Pamplona Street, 47, Barcelona, 08088</p>
+        </div>
+        <div>
+          <h3>Legal</h3>
+          <p>Terms & Conditions</p>
+          <p>Privacy</p>
+          <p>Coockie Policy</p>
+        </div>
+        <div className="socialFooter">
+          <h3 >Social</h3>
+          <img width="30px" src={IGImg} alt="Instagram" />
+          <img width="30px" src={tiktokImg} alt="TikTok" />
+          <img width="30px" src={linkedinImg} alt="LinkdIn" />
+          <img width="30px" src={FBImg} alt="Facebook " />
+        </div>
+      </div>
+      <p>2023 EcoUpper. All rights reserved</p>
+      <Link to="/About">About Us</Link>
+    </footer>
+  );
+};
 
-export default Footer; 
+export default Footer;
